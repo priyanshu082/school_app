33d66be649bfe23d8139538d4370a78f244bf081
priyanshu082 <priyanshusingh216@gmail.com>
2025-01-06 22:16:05 +0530

    FORM IS DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import Form from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='bg-yellow-200'>
-      Priyanshu
+    <div className='flex justify-center items-center'>
+
+    <Form/>
     </div>
     </>
   )
